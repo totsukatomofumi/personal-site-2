25f5e2082d2a7b9b94fdc0fdb89129684bdd07e7
totsukatomofumi <totsukatomofumi@gmail.com>
2025-10-11 22:00:37 +0800

    Add text outline

--- src/components/Text/index.jsx
+++ src/components/Text/index.jsx
@@ -254,7 +254,7 @@ function Text({ className, ...props }) {
         {/* =================== Content =================== */}
         <div
           ref={content.current.ref}
-          className={`w-1/2 max-w-screen flex flex-col text-xl leading-12 perspective-normal ${
+          className={`w-1/2 max-w-screen flex flex-col text-xl text-shadow-[-1px_-1px_0_var(--color-background),_1px_-1px_0_var(--color-background),_-1px_1px_0_var(--color-background),_1px_1px_0_var(--color-background)] leading-12 perspective-normal ${
             isDevShowTextOverlay ? "border-1 border-blue-500" : ""
           }`}
         >
